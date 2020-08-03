261972ba1b1fcac338b188cc0a6d9d2d47f19bb7
Alexandru Cohal <alexandru.cohal@yahoo.com>
2020-08-03 14:09:54 +0200

    The BitmapInfoHeader.h file which contains the information about the Bitmap data written in the Bitmap file was added

---
+++ src/BitmapInfoHeader.h
@@ -0,0 +1,30 @@
+/*
+ * This file contains the structure BitmapInfoHeader which has the information
+ * about the Bitmap data contained in the file.
+ */
+
+#ifndef BITMAPINFOHEADER_H_
+#define BITMAPINFOHEADER_H_
+
+#include <cstdint>
+using namespace std;
+
+// Aligns structure members on 2-byte boundaries
+#pragma pack(2)
+
+	struct BitmapInfoHeader
+	{
+		int32_t headerSize{40}; // The size of the struct (bytes)
+		int32_t width; // The width of the bitmap
+		int32_t height; // The height of the bitmap
+		int32_t planes{1};
+		int32_t bitsPerPixel{24}; // One byte for each color (R, G, B)
+		int32_t compression{0}; // 0 = No compression
+		int32_t dataSize{0}; // The size of the data written in the file
+		int32_t horizontalResolution{2400};
+		int32_t verticalResolution{2400};
+		int32_t colors{0};
+		int32_t importantColors{0};
+	};
+
+#endif /* BITMAPINFOHEADER_H_ */
