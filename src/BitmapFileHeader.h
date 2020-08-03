d7695a22087523b6c2654de7c1e09dc540f69527
Alexandru Cohal <alexandru.cohal@yahoo.com>
2020-08-03 13:59:25 +0200

    The BitmapFileHeader.h file which contains the data (packed in a structure) which has to be written at the beginning of a Bitmap file was added

---
+++ src/BitmapFileHeader.h
@@ -0,0 +1,23 @@
+/*
+ * This file contains the structure BitmapFileHeader which has the data which has
+ * to be written at the beginning of a Bitmap file in order to be a valid file.
+ */
+
+#ifndef BITMAPFILEHEADER_H_
+#define BITMAPFILEHEADER_H_
+
+#include <cstdint>
+using namespace std;
+
+// Aligns structure members on 2-byte boundaries
+#pragma pack(2)
+
+	struct BitmapFileHeader
+	{
+		char header[2]{'B', 'M'};
+		int32_t fileSize; // The size of the final file
+		int32_t reserved{0};
+		int32_t dataOffset; // Where the actual data begins is the file
+	};
+
+#endif /* BITMAPFILEHEADER_H_ */
