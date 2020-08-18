7a2360083392420fe01bdfc92028ae95cff43d0b
Alexandru Cohal <alexandru.cohal@yahoo.com>
2020-08-18 23:42:55 +0200

    The Mandelbrot class was added

---
+++ src/Mandelbrot.cpp
@@ -0,0 +1,15 @@
+#include "Mandelbrot.h"
+
+Mandelbrot::Mandelbrot() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Mandelbrot::~Mandelbrot() {
+	// TODO Auto-generated destructor stub
+}
+
+int Mandelbrot::getIterations(double x, double y)
+{
+	return 0;
+}
