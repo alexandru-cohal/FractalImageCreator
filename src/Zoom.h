4a5c5dd9253f5df1c6a0919853ec812fcb903f69
Alexandru Cohal <alexandru.cohal@yahoo.com>
2020-08-25 23:36:38 +0200

    Centering and scaling added to the 'add' method of the ZoomList class

--- src/Zoom.h
+++ src/Zoom.h
@@ -7,6 +7,7 @@
 
 class Zoom
 {
+public:
 	int x{0};
 	int y{0};
 	double scale{0.0};
--- src/ZoomList.cpp
+++ src/ZoomList.cpp
@@ -11,6 +11,10 @@ ZoomList::ZoomList(int width, int height): m_width(width), m_height(height)
 void ZoomList::add(const Zoom& zoom)
 {
 	zooms.push_back(zoom);
+
+	m_xCenter += (zoom.x - m_width / 2) * m_scale;
+	m_yCenter += (zoom.y - m_height / 2) * m_scale;
+	m_scale *= zoom.scale;
 }
 
 pair<double, double> ZoomList::doZoom(int x, int y)
