18f1197c1a511c1e5495efd6ee44e730a30848d4
Alexandru Cohal <alexandru.cohal@yahoo.com>
2020-08-25 23:10:22 +0200

    The colors of the fractal's pixels were improved (the interior of the fractal is set to black)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdint>
 #include <memory>
+#include <math.h>
 #include "Bitmap.h"
 #include "Mandelbrot.h"
 using namespace std;
@@ -43,18 +44,23 @@ int main()
 	{
 		for (int x = 0; x < WIDTH; x++)
 		{
+			uint8_t red = 0;
+			uint8_t green = 0;
+			uint8_t blue = 0;
+
 			int iterations = fractal[y * WIDTH + x];
 
-			double hue = 0;
-
-			for (int i = 0; i <= iterations; i++)
+			if (iterations != Mandelbrot::MAX_ITERATIONS)
 			{
-				hue += ((double)histogram[i]) / total;
-			}
+				double hue = 0;
 
-			uint8_t red = 0;
-			uint8_t green = hue * 255;
-			uint8_t blue = 0;
+				for (int i = 0; i <= iterations; i++)
+				{
+					hue += ((double)histogram[i]) / total;
+				}
+
+				green = pow(255, hue);
+			}
 
 			bitmap.setPixel(x, y, red, green, blue);
 		}
